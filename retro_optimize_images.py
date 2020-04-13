9d742000977ec73dc7e1167d7d05af45787242ef
Jake Ledoux <jaik.exe@gmail.com>
2020-04-12 21:40:17 -0400

    optimized images are now forced jpeg

--- retro_optimize_images.py
+++ retro_optimize_images.py
@@ -5,4 +5,4 @@ for file in os.listdir("static/img/user_uploads"):
     filename = os.path.join("static/img/user_uploads", file)
     if os.path.isfile(filename):
         print(f"Optimizing: {filename}")
-        turtle_images.prep_and_save(filename, filename)
+        turtle_images.prep_and_save(filename, os.path.splitext(filename)[0] + ".jpg")
