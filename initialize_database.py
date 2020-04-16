f4cb281f42798580a6cbc06eb83f83666d9f949b
Jake Ledoux <jaik.exe@gmail.com>
2020-04-16 10:14:56 -0400

    yikes

--- initialize_database.py
+++ initialize_database.py
@@ -6,21 +6,21 @@ db.create_all()
 # Create users
 jake = Crab.create_new(username="jake",
                        email="contactjakeledoux@gmail.com",
-                       password="***REMOVED***",
+                       password="fish",
                        display_name="Jake Ledoux",
                        verified=True,
                        bio="I made this site.")
 
 crabber = Crab.create_new(username="crabber",
                           email="crabberwebsite@gmail.com",
-                          password="***REMOVED***",
+                          password="fish",
                           display_name="Crabber",
                           verified=True,
                           bio="Official account for website news and updates.")
 
 test_account = Crab.create_new(username="test_account",
                                email="jaik.exe@gmail.com",
-                               password="***REMOVED***",
+                               password="fish",
                                display_name="Test Account",
                                verified=False,
                                bio="just testing around")
