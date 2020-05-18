02d2ef767412d5621f0050d9f49439aba7f9c75b
Jake <contactjakeledoux@gmail.com>
2020-05-18 12:02:32 -0400

    Update initialize_database.py

--- initialize_database.py
+++ initialize_database.py
@@ -3,24 +3,10 @@ from crabber import db, Crab, Molt
 db.drop_all()
 db.create_all()
 
-# Create users
-jake = Crab.create_new(username="jake",
-                       email="contactjakeledoux@gmail.com",
-                       password="fish",
-                       display_name="Jake Ledoux",
-                       verified=True,
-                       bio="I made this site.")
-
+# Create crabber account
 crabber = Crab.create_new(username="crabber",
                           email="crabberwebsite@gmail.com",
                           password="fish",
                           display_name="Crabber",
                           verified=True,
                           bio="Official account for website news and updates.")
-
-test_account = Crab.create_new(username="test_account",
-                               email="jaik.exe@gmail.com",
-                               password="fish",
-                               display_name="Test Account",
-                               verified=False,
-                               bio="just testing around")
