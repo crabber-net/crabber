e8a39d822779d732bf569d003103bbc2b6b1c2fb
Jake Ledoux <contactjakeledoux@gmail.com>
2020-05-31 08:38:42 -0400

    Database now initializes with trophies

--- initialize_database.py
+++ initialize_database.py
@@ -1,4 +1,4 @@
-from crabber import db, Crab, Molt
+from crabber import db, Crab, Molt, Trophy
 
 db.drop_all()
 db.create_all()
@@ -10,3 +10,39 @@ crabber = Crab.create_new(username="crabber",
                           display_name="Crabber",
                           verified=True,
                           bio="Official account for website news and updates.")
+
+# Initialize trophies
+trophies = [
+    Trophy(title="Social Newbie",
+           description="Get your first follower"),
+    Trophy(title="Mingler",
+           description="Have 10 followers"),
+    Trophy(title="Life of the Party",
+           description="Have 100 followers"),
+    Trophy(title="Celebrity",
+           description="Have 1000 followers"),
+    Trophy(title="Baby Crab",
+           description="Publish your first Molt"),
+    Trophy(title="Pineapple Express",
+           description="Use the crabtag %420"),
+    Trophy(title="I Want it That Way",
+           description="Change your avatar, banner, and bio"),
+    Trophy(title="Dopamine Hit",
+           description="Get 10 likes on a Molt"),
+    Trophy(title="Dopamine Addict",
+           description="Get 100 likes on a Molt. Need... more..."),
+    Trophy(title="Full on Junkie",
+           description="Get 1,000 likes on a Molt"),
+    Trophy(title="Back-Crabber",
+           description="Follow someone back only to have them unfollow you"),
+    Trophy(title="I Captivated the Guy",
+           description="Be followed by a verified user"),
+    Trophy(title="Lab Rat",
+           description="Participate in Crabber's beta phase"),
+    Trophy(title="They Live",
+           description="Survive the great site reset of 2020"),
+]
+for trophy in trophies:
+    db.session.add(trophy)
+
+db.session.commit()
