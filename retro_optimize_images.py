74253355f3b4c10c734d211908f3bf07246a5b7f
Jake Ledoux <contactjakeledoux@gmail.com>
2020-05-31 13:02:19 -0400

    Further portability improvements

--- retro_optimize_images.py
+++ retro_optimize_images.py
@@ -1,8 +1,9 @@
 import os
 import turtle_images
 
-for file in os.listdir("static/img/user_uploads"):
-    filename = os.path.join("static/img/user_uploads", file)
+user_uploads = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static/img/user_uploads')
+for file in os.listdir(user_uploads):
+    filename = os.path.join(user_uploads, file)
     if os.path.isfile(filename):
-        print(f"Optimizing: {filename}")
-        turtle_images.prep_and_save(filename, os.path.splitext(filename)[0] + ".jpg")
+        print(f'Optimizing: {filename}')
+        turtle_images.prep_and_save(filename, os.path.splitext(filename)[0] + '.jpg')
