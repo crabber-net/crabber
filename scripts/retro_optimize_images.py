e901ea0fea123722e997c6e3c09b7d4211c46dd0
Jake Ledoux <contactjakeledoux@gmail.com>
2021-02-01 13:37:31 -0500

    Fixed script imports

--- scripts/retro_optimize_images.py
+++ scripts/retro_optimize_images.py
@@ -1,4 +1,8 @@
-import os
+import os,sys,inspect
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0,parentdir)
+
 import turtle_images
 
 user_uploads = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'static/img/user_uploads')
--- scripts/test.py
+++ scripts/test.py
@@ -1,3 +1,8 @@
+import os,sys,inspect
+currentdir = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+parentdir = os.path.dirname(currentdir)
+sys.path.insert(0,parentdir)
+
 from crabber import db, Crab, Molt
 
 db.drop_all()
