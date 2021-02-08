3906bbb993bb0d38f582ad8761df204e0402d540
Jake Ledoux <contactjakeledoux@gmail.com>
2021-02-08 09:32:42 -0500

    Fixed invalid social_title bug

--- crabber.py
+++ crabber.py
@@ -346,14 +346,16 @@ def molt_page(username, molt_id):
     # Display page
     else:
         primary_molt = models.Molt.get_by_ID(molt_id)
-        social_title = f'{primary_molt.author.display_name}\'s post on Crabber'
         ajax_content = request.args.get('ajax_content')
         if primary_molt is None:
+            social_title = f'Unavailable Post'
             return render_template('not-found.html', current_user=utils.get_current_user(), noun="molt")
         elif primary_molt.author.banned:
+            social_title = f'Unavailable Post'
             return render_template('not-found.html', current_user=utils.get_current_user(),
                                    message='The author of this Molt has been banned.')
         else:
+            social_title = f'{primary_molt.author.display_name}\'s post on Crabber'
             replies = primary_molt.query_replies()
             return render_template('molt-page-replies.html' if ajax_content else 'molt-page.html', current_page="molt-page", molt=primary_molt,
                                    replies=replies, current_user=utils.get_current_user(),
