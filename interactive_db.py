d4bd9ce91287a740853a2851e0ca1902013f6fe9
Jake Ledoux <contactjakeledoux@gmail.com>
2021-11-06 14:49:05 -0400

    Added ModLog to imports

--- interactive_db.py
+++ interactive_db.py
@@ -2,7 +2,7 @@ from crabber import app
 from extensions import db
 from models import AccessToken, Bookmark, crabtag_table, Card, Crab, Crabtag, \
         DeveloperKey, following_table, Like, Molt, Trophy, TrophyCase, \
-        Notification, blocking_table
+        ModLog, Notification, blocking_table
 from sqlalchemy import desc, func, or_
 
 app.app_context().push()
