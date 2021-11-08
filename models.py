8781741b644b85872dec4898d9e2fb7f3cdb33b8
Jake Ledoux <contactjakeledoux@gmail.com>
2021-11-08 10:26:13 -0500

    Permanently remove unfollow notification

--- models.py
+++ models.py
@@ -615,10 +615,6 @@ class Crab(db.Model):
         """
         if crab in self._following and crab is not self:
             self._following.remove(crab)
-            #
-            # Temporarily disabled due to spamming:
-            #
-            # crab.notify(sender=self, type="unfollow")
             db.session.commit()
 
     def verify_password(self, password):
