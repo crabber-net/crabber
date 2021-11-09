d543aeed50bfe2f92f7f9d9ea8b8eef8cbb52bab
Jake Ledoux <contactjakeledoux@gmail.com>
2021-11-09 16:10:27 -0500

    Preserve formatting for specific patterns

--- patterns.py
+++ patterns.py
@@ -13,9 +13,18 @@ youtube = re.compile(
 )
 giphy = re.compile(r"https://(?:media\.)?giphy\.com/\S+[-/](\w{13,21})(?:\S*)")
 ext_img = re.compile(r"(https://\S+\.(gif|jpe?g|png))(?:\s|$)")
-link_basic = r"(?:https?://)\S+" r"|" r"(?:www\.)?\w{3,}\.(?i:com|net|org|gov|io)"
-ext_link = re.compile(r"(?<!href=['\"])(?:^|\s|[(])" r"(" + link_basic + r")")
-ext_md_link = re.compile(rf"\[([^\]\(\)]+)\]\(({link_basic})" r"\)")
+# fmt: off
+link_basic = (
+    r"(?:https?://)\S+"
+    r"|"
+    r"(?:www\.)?\w{3,}\.(?i:com|net|org|gov|io)"
+)
+ext_link = re.compile(
+    r"(?<!href=['\"])(?:^|\s|[(])"
+    r"(" + link_basic + r")"
+)
+# fmt: on
+ext_md_link = re.compile(rf"\[([^\]\(\)]+)\]\(({link_basic})\)")
 timezone = re.compile(r"^-?(1[0-2]|0[0-9]).\d{2}$")
 pretty_url = re.compile(r"(?:https?://)?(?:www\.)?((?:(?:[\w_-]+\.?)+/?)+)")
 # Captures root of url (e.g. reddit.com or crabber.net)
