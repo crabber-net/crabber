20de2355bdd97e9de9f729ea745b48cc4543c2c6
Jake Ledoux <contactjakeledoux@gmail.com>
2021-11-09 15:00:21 -0500

    Re-implemented advanced link detection

--- patterns.py
+++ patterns.py
@@ -13,12 +13,16 @@ youtube = re.compile(
 )
 giphy = re.compile(r"https://(?:media\.)?giphy\.com/\S+[-/](\w{13,21})(?:\S*)")
 ext_img = re.compile(r"(https://\S+\.(gif|jpe?g|png))(?:\s|$)")
-ext_link = re.compile(
-    r'(?<!href=[\'"])('
+link_basic = (
     r"(?:https?://)\S+"
-    r")"
+    r"|"
+    r"(?:www\.)?\w{3,}\.(?i:com|net|org|gov|io)"
 )
-ext_md_link = re.compile(rf"\[([^\]\(\)]+)\]\(({ext_link.pattern})" r"\)")
+ext_link = re.compile(
+    r"(?<!href=['\"])(?:^|\s|[(])"
+    r"(" + link_basic + r")"
+)
+ext_md_link = re.compile(rf"\[([^\]\(\)]+)\]\(({link_basic})" r"\)")
 timezone = re.compile(r"^-?(1[0-2]|0[0-9]).\d{2}$")
 pretty_url = re.compile(r"(?:https?://)?(?:www\.)?((?:(?:[\w_-]+\.?)+/?)+)")
 # Captures root of url (e.g. reddit.com or crabber.net)
